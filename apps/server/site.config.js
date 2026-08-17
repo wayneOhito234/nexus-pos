71be85aafb46dc1678acfa0ed72996414d8a6d79
ale-ship <alexandersomba17@gmail.com>
2026-08-17 07:38:14 -0700

    updated till ip on site.config.js with real till ips

--- apps/server/site.config.js
+++ apps/server/site.config.js
@@ -2,15 +2,15 @@ export const siteConfig = {
   // Shown on startup and served to the manager app via GET /api/site.
   siteName: 'Zummart Supermarket',
 
-  serverIp: '192.168.100.16',
+  serverIp: '192.168.100.11',
 
   tillIps: [
-    '192.168.100.28',
-    '192.168.100.29',
+    '192.168.100.9',
+    '192.168.100.17',
   ],
 
   managerIps: [
-    '192.168.100.16',
+    '192.168.100.11',
   ],
 
   allowSubnet: '192.168.100.0/24',
